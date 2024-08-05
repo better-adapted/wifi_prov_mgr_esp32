a05f1aaa528cde9e92419208d98d8bb14bb72153
Adam Watson <adam_t_watson@hotmail.com>
2024-08-05 10:14:30 +0100

    added bacth file to test openocd

---
+++ openocd_start.bat
@@ -0,0 +1,1 @@
+f:\Espressif\tools\openocd-esp32\v0.12.0-esp32-20230921\openocd-esp32\bin/openocd.exe -s ${openocd_path}/share/openocd/scripts -f interface/ftdi/esp32_devkitj_v1.cfg -f target/esp32s3.cfg
