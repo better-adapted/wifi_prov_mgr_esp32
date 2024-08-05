6fab9cfd60d5871ef797470bebcb580bfaafb596
better-adapted <better-adapted@users.noreply.github.com>
2024-08-05 13:58:36 +0100

    online on the i7 ideapad

--- openocd_start.bat
+++ openocd_start.bat
@@ -1,1 +1,3 @@
-f:\Espressif\tools\openocd-esp32\v0.12.0-esp32-20230921\openocd-esp32\bin/openocd.exe -s ${openocd_path}/share/openocd/scripts -f interface/ftdi/esp32_devkitj_v1.cfg -f target/esp32s3.cfg
+REM f:\Espressif\tools\openocd-esp32\v0.12.0-esp32-20230921\openocd-esp32\bin/openocd.exe -s ${openocd_path}/share/openocd/scripts -f interface/ftdi/esp32_devkitj_v1.cfg -f target/esp32s3.cfg
+D:\Espressif\tools\openocd-esp32\v0.12.0-esp32-20230921\openocd-esp32\bin/openocd.exe -s ${openocd_path}/share/openocd/scripts -f interface/ftdi/esp32_devkitj_v1.cfg -f target/esp32.cfg
+REM D:\Espressif\tools\openocd-esp32\v0.12.0-esp32-20230921\openocd-esp32\bin/openocd.exe -s ${openocd_path}/share/openocd/scripts -f interface/ftdi/esp32_devkitj_v1.cfg -f target/esp32s3.cfg
